baa76925cef2f04f5948c669fa89ef032e0c980b
MiaoDragon <innocentmyl@gmail.com>
2019-06-27 17:16:17 -0700

    delete printing stuff

--- point_cloud_dataset.py
+++ point_cloud_dataset.py
@@ -47,14 +47,14 @@ class GazeboSceneModifier():
         #todo
 
     def spawn_service_call(self, model, file, pose, z_offset):
-        print('spawning model:')
-        print(model)
-        print('spawning file:')
-        print(file)
-        print('pose:')
-        print(pose)
-        print('z_offset:')
-        print(z_offset)
+        #print('spawning model:')
+        #print(model)
+        #print('spawning file:')
+        #print(file)
+        #print('pose:')
+        #print(pose)
+        #print('z_offset:')
+        #print(z_offset)
         orientation = list(tf.transformations.quaternion_from_euler(0, 0, 0))
         orient_fixed = Quaternion(
             orientation[0], orientation[1], orientation[2], orientation[3])
