c8e8a27a57f1e00e551d2b15f9ec1fec55106c1b
MiaoDragon <innocentmyl@gmail.com>
2019-07-01 16:31:05 -0700

    shouldn't use permutation

--- train_AE.py
+++ train_AE.py
@@ -122,7 +122,7 @@ def main(args):
             decoder.zero_grad()
             # convert to pytorch tensors and Varialbes
             bobs = torch.from_numpy(obstacles[i:i+args.batch_size]).type(torch.FloatTensor)
-            bobs = to_var(bobs).view(len(bobs), -1, 3).permute(0,2,1)
+            #bobs = to_var(bobs).view(len(bobs), -1, 3).permute(0,2,1)
             print('data:')
             print(bobs)
             # forward pass through encoder
