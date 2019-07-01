8d614b2a7e577a4c4eaf6203b72a42d1be07524c
MiaoDragon <innocentmyl@gmail.com>
2019-07-01 15:08:21 -0700

    chamfer

--- architectures/pytorch/utils/chamfer/dist_chamfer.py
+++ architectures/pytorch/utils/chamfer/dist_chamfer.py
@@ -5,7 +5,7 @@ import torch
 import sys
 from numbers import Number
 from collections import Set, Mapping, deque
-import architectures.pytorch.utils.chamfer
+import architectures.pytorch.utils.chamfer as chamfer
 
 
 # Chamfer's distance module @thibaultgroueix
