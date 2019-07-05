7860ca0d2ae3043d2f9a8af235e2f5ffa53598a4
MiaoDragon <innocentmyl@gmail.com>
2019-07-05 14:10:02 -0700

    no try

--- tools/obs_data_loader.py
+++ tools/obs_data_loader.py
@@ -15,13 +15,12 @@ def pool_pc_length_check(args):
     return importer.pointcloud_length_check(pcd_fname=pcd_data_path + fname)
 
 def pool_pc_load(args):
-    try:
-        fname, min_length, importer, pcd_data_path = args
-        data = importer.pointcloud_import(pcd_fname=pcd_data_path + fname)[:min_length]
-        return data
-    except:
-        return None
-		
+    fname, min_length, importer, pcd_data_path = args
+    data = importer.pointcloud_import(pcd_fname=pcd_data_path + fname)[:min_length]
+    return data
+    #except:
+    #    return None
+
 def load_dataset(env_names,pcd_data_path,importer,min_length=(5351*3)):
     """
     Load point cloud dataset into array of obstacle pointclouds, which will be entered as input to the encoder NN
