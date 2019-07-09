45a363e3d9f891ad9aaf14fb79c432f70e8227b2
MiaoDragon <innocentmyl@gmail.com>
2019-07-08 23:25:21 -0700

    learning_rate variables

--- architectures/mpnet.py
+++ architectures/mpnet.py
@@ -19,7 +19,7 @@ from . general_utils import apply_augmentations
 class Configuration():
     def __init__(self, n_o_input, n_x_input, n_output, encoder, decoder, mlp, pretrain,
                  pretrain_epoch, pretrain_batch_size, fixAE, encoder_args={}, decoder_args={}, mlp_args={},
-                 training_epochs=200, batch_size=10, learning_rate=0.001, denoising=False,
+                 training_epochs=200, batch_size=10, ae_learning_rate=0.001, mlp_learning_rate=0.001, denoising=False,
                  saver_step=None, train_dir=None, z_rotate=False, loss='chamfer', gauss_augment=None,
                  saver_max_to_keep=None, loss_display_step=1, debug=False,
                  latent_vs_recon=1.0, consistent_io=None):
