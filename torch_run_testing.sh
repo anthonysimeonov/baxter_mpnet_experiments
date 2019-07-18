2fd3b2f1b9373eeeb86d7edbfad82e939497a59a
MiaoDragon <innocentmyl@gmail.com>
2019-07-18 12:22:10 -0700

    use test data path

--- torch_run_testing.sh
+++ torch_run_testing.sh
@@ -1,4 +1,4 @@
 python torch_test.py \
---env_data_path ./env/environment_data/ --path_data_path ./data/test/paths/ --pointcloud_data_path ./data/test/pcd/ \
+--env_data_path ./env/environment_data/ --path_data_path ./test_data/test/paths/ --pointcloud_data_path ./test_data/test/pcd/ \
 --envs_file trainEnvironments.pkl --path_data_file trainEnvironments_testPaths.pkl \
 --model_path ./models/sample/ --mlp_model_name mlp_PReLU_ae_dd0.pkl --enc_model_name cae_encoder_0.pkl --experiment_name test_experiment
