d28855cc737a523a73becd300bd8f0a2d712a1e7
MiaoDragon <innocentmyl@gmail.com>
2019-07-17 17:01:18 -0700

    try using 0 cuda device

--- run_testing_linear.sh
+++ run_testing_linear.sh
@@ -5,4 +5,4 @@ python test.py \
 --batch_size 100 --ae_learning_rate 0.0005 --mlp_learning_rate 0.0005 --num_epochs 200 \
 --enc_input_size 16053 --enc_output_size 60 --mlp_input_size 74 --mlp_output_size 7 --start_epoch 10 \
 --AE_start_epoch 10 --AE_restore_pretrain 0 --pretrain 0 --pretrain_epochs 500 --pretrain_batch_size 50 \
---fixAE 0 --experiment_name no_pretrain_linear --device 3 --AE_type linear \
+--fixAE 0 --experiment_name no_pretrain_linear --device 0 --AE_type linear \
