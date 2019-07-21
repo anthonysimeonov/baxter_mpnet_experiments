d3432413e1869813bb54add231f38986a8e12946
MiaoDragon <innocentmyl@gmail.com>
2019-07-20 18:47:54 -0700

    alpha should be trainable

--- architectures/mlp_models.py
+++ architectures/mlp_models.py
@@ -6,9 +6,9 @@ import tensorflow as tf
 import tensorflow as tf
 def wrap_prelu(_x, name=None):
     # ref: https://stackoverflow.com/questions/39975676/how-to-implement-prelu-activation-in-tensorflow
-    alphas = tf.get_variable(name, _x.get_shape()[-1],
+    alphas = tf.get_variable(name, [1],
                            initializer=tf.constant_initializer(0.25),
-                            dtype=tf.float32)
+                            dtype=tf.float32, trainable=True)
     pos = tf.nn.relu(_x)
     neg = alphas * (_x - abs(_x)) * 0.5
     return pos + neg
