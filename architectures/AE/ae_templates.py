44f045bc975a35129b2da474d69e578112f0b3e3
MiaoDragon <innocentmyl@gmail.com>
2019-07-29 13:15:41 -0700

    try using shallower layer for pointnet

--- architectures/AE/ae_templates.py
+++ architectures/AE/ae_templates.py
@@ -35,7 +35,7 @@ def mlp_architecture_ala_iclr_18(n_pc_points, bneck_size, bneck_post_mlp=False):
                     'verbose': True
                     }
     """
-    encoder_args = {'n_filters': [64, 128, 128, 256, bneck_size],
+    encoder_args = {'n_filters': [64, 128, bneck_size],
                     'filter_sizes': [1],
                     'strides': [1],
                     'b_norm': False,
