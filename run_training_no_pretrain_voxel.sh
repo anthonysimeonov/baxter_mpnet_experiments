1bd18f22cc876068c4e8e253a229213b351e9399
MiaoDragon <innocentmyl@gmail.com>
2019-08-01 18:27:59 -0700

    1000 np

--- run_training_no_pretrain_voxel.sh
+++ run_training_no_pretrain_voxel.sh
@@ -5,4 +5,4 @@ python train.py \
 --batch_size 100 --ae_learning_rate 0.001 --mlp_learning_rate 0.001 --num_epochs 200 \
 --enc_input_size 32 --enc_output_size 128 --mlp_input_size 142 --mlp_output_size 7 --start_epoch 0 \
 --AE_start_epoch 0 --AE_restore_pretrain 0 --pretrain 0 --pretrain_epochs 500 --pretrain_batch_size 50 \
---fixAE 0 --experiment_name no_pretrain_voxel --device 0 --AE_type voxelnet --NP 100
+--fixAE 0 --experiment_name no_pretrain_voxel --device 0 --AE_type voxelnet --NP 1000
