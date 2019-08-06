e65cdc9332a5c10fe6ffa6eda15958bb62e9eb7c
MiaoDragon <innocentmyl@gmail.com>
2019-08-05 17:34:18 -0700

    tabatab

--- torch_architectures.py
+++ torch_architectures.py
@@ -7,9 +7,9 @@ from torch.autograd import Variable
 
 # DMLP Model-Path Generator
 class MLP(nn.Module):
-	def __init__(self, input_size, output_size):
-		super(MLP, self).__init__()
-		self.fc = nn.Sequential(
+    def __init__(self, input_size, output_size):
+        super(MLP, self).__init__()
+        self.fc = nn.Sequential(
                     nn.Linear(input_size, 1280), nn.PReLU(), nn.Dropout(),
                     nn.Linear(1280, 896), nn.PReLU(), nn.Dropout(),
                     nn.Linear(896, 512), nn.PReLU(), nn.Dropout(),
@@ -20,49 +20,49 @@ class MLP(nn.Module):
                     nn.Linear(64, 32), nn.PReLU(),
                     nn.Linear(32, output_size))
 
-	def forward(self, x):
-		out = self.fc(x)
-		return out
+    def forward(self, x):
+        out = self.fc(x)
+        return out
 
 
 class MLP_Path(nn.Module):
- 	def __init__(self, input_size, output_size):
- 		super(MLP_Path, self).__init__()
- 		self.fc = nn.Sequential(
+     def __init__(self, input_size, output_size):
+         super(MLP_Path, self).__init__()
+         self.fc = nn.Sequential(
                     nn.Linear(input_size, 512), nn.PReLU(), nn.Dropout(),
                     nn.Linear(512, 512), nn.PReLU(), nn.Dropout(),
                     nn.Linear(512, output_size))
 
- 	def forward(self, x):
- 		out = self.fc(x)
- 		return out
+     def forward(self, x):
+         out = self.fc(x)
+         return out
 
 
 class VoxelEncoder(nn.Module):
-	# ref: https://github.com/lxxue/voxnet-pytorch/blob/master/models/voxnet.py
-	def __init__(self, input_size, output_size):
-		super(VoxelEncoder, self).__init__()
-		self.encoder = nn.Sequntial(
-			nn.Conv3d(in_channels=1, out_channels=32, kernel_size=[5,5,5], stride=[2,2,2]),
-			nn.PReLU(),
-			nn.Conv3d(in_channels=32, out_channels=32, kernel_size=[3,3,3], stride=[1,1,1]),
-			nn.PReLU(),
-			nn.MaxPool3d(kernel_size=[2,2,2], stride=[2,2,2])
-		)
+    # ref: https://github.com/lxxue/voxnet-pytorch/blob/master/models/voxnet.py
+    def __init__(self, input_size, output_size):
+        super(VoxelEncoder, self).__init__()
+        self.encoder = nn.Sequntial(
+            nn.Conv3d(in_channels=1, out_channels=32, kernel_size=[5,5,5], stride=[2,2,2]),
+            nn.PReLU(),
+            nn.Conv3d(in_channels=32, out_channels=32, kernel_size=[3,3,3], stride=[1,1,1]),
+            nn.PReLU(),
+            nn.MaxPool3d(kernel_size=[2,2,2], stride=[2,2,2])
+        )
         x = self.encoder(torch.autograd.Variable(torch.rand((1, 1) + input_shape)))
-		first_fc_in_features = 1
-    	for n in x.size()[1:]:
-        	first_fc_in_features *= n
-		self.head = nn.Sequential(
-			nn.Linear(first_fc_in_features, 128),
-			nn.PReLU(),
-			nn.Linear(128, output_size)
-		)
-	def forward(self, x):
-		x = self.encoder(x)
-		x = x.view(x.size(0), -1)
-		x = self.head(x)
-		return x
+        first_fc_in_features = 1
+        for n in x.size()[1:]:
+            first_fc_in_features *= n
+        self.head = nn.Sequential(
+            nn.Linear(first_fc_in_features, 128),
+            nn.PReLU(),
+            nn.Linear(128, output_size)
+        )
+    def forward(self, x):
+        x = self.encoder(x)
+        x = x.view(x.size(0), -1)
+        x = self.head(x)
+        return x
 
 
 
