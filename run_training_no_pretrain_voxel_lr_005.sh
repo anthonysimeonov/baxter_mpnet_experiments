0630059edf6e32fc1800e9f6112a9b23dd500249
MiaoDragon <innocentmyl@gmail.com>
2019-08-05 17:12:07 -0700

    change start epoch to 0

--- run_training_no_pretrain_voxel_lr_005.sh
+++ run_training_no_pretrain_voxel_lr_005.sh
@@ -3,6 +3,6 @@ python train.py \
 --envs_file trainEnvironments.pkl --path_data_file trainPaths.pkl \
 --trained_model_path ./models/sample/ \
 --batch_size 100 --ae_learning_rate 0.005 --mlp_learning_rate 0.005 --num_epochs 500 \
---enc_input_size 32 --enc_output_size 128 --mlp_input_size 142 --mlp_output_size 7 --start_epoch 200 \
---AE_start_epoch 200 --AE_restore_pretrain 0 --pretrain 0 --pretrain_epochs 500 --pretrain_batch_size 50 \
+--enc_input_size 32 --enc_output_size 128 --mlp_input_size 142 --mlp_output_size 7 --start_epoch 0 \
+--AE_start_epoch 0 --AE_restore_pretrain 0 --pretrain 0 --pretrain_epochs 500 --pretrain_batch_size 50 \
 --fixAE 0 --experiment_name no_pretrain_voxel_005 --device 3 --AE_type voxelnet --NP 100
