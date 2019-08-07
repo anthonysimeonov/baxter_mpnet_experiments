3154b99493108fddcb026a8ac8c0596df2a2ce92
MiaoDragon <innocentmyl@gmail.com>
2019-08-07 00:16:40 -0700

    added other types

--- torch_test.py
+++ torch_test.py
@@ -60,6 +60,22 @@ def main(args):
         # convert obstacles to voxel
         obstacles = obstacles.astype(float).reshape(len(obstacles),-1,3)
         obstacles = importer.pointcloud_to_voxel(obstacles).reshape(len(obstacles),1,args.enc_input_size,args.enc_input_size,args.enc_input_size)
+    elif args.AE_type == 'voxel2':
+        encoder = VoxelEncoder2(args.enc_input_size, args.enc_output_size)
+        # convert obstacles to voxel
+        obstacles = obstacles.astype(float).reshape(len(obstacles),-1,3)
+        obstacles = importer.pointcloud_to_voxel(obstacles).reshape(len(obstacles),1,args.enc_input_size,args.enc_input_size,args.enc_input_size)
+    elif args.AE_type == 'voxel3':
+        encoder = VoxelEncoder3(args.enc_input_size, args.enc_output_size)
+        # convert obstacles to voxel
+        obstacles = obstacles.astype(float).reshape(len(obstacles),-1,3)
+        obstacles = importer.pointcloud_to_voxel(obstacles).reshape(len(obstacles),1,args.enc_input_size,args.enc_input_size,args.enc_input_size)
+    elif args.AE_type == 'voxel4':
+        encoder = VoxelEncoder3(args.enc_input_size, args.enc_output_size)
+        # convert obstacles to voxel
+        obstacles = obstacles.astype(float).reshape(len(obstacles),-1,3)
+        obstacles = importer.pointcloud_to_voxel(obstacles).reshape(len(obstacles),1,args.enc_input_size,args.enc_input_size,args.enc_input_size)
+
     mlp = MLP(args.mlp_input_size, args.mlp_output_size)
 
     # device = torch.device('cpu')
