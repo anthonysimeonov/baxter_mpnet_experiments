bfeb7b6179db7874764c8d076e35c763dacafb81
MiaoDragon <innocentmyl@gmail.com>
2019-08-09 15:23:09 -0700

    device

--- torch_run_testing.sh
+++ torch_run_testing.sh
@@ -1,4 +1,4 @@
 python torch_test.py \
 --env_data_path ./env/environment_data/ --path_data_path ./test_data/test/paths/ --pointcloud_data_path ./test_data/test/pcd/ \
 --envs_file trainEnvironments.pkl --path_data_file trainEnvironments_testPaths.pkl \
---model_path ./models/sample/linear/ --mlp_model_name mlp_PReLU_ae_dd120.pkl --enc_model_name cae_encoder_120.pkl --experiment_name test_experiment
+--model_path ./models/sample/linear/ --mlp_model_name mlp_PReLU_ae_dd160.pkl --enc_model_name cae_encoder_160.pkl --experiment_name test_experiment --device 2
