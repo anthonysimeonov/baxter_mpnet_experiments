f53d861668c9e9e815b8f4b3222b58c5851c316e
MiaoDragon <innocentmyl@gmail.com>
2019-08-12 14:35:20 -0700

    try increasing the resolution of voxels

--- torch_run_training_voxel_001_1000.sh
+++ torch_run_training_voxel_001_1000.sh
@@ -3,5 +3,5 @@ python torch_train.py \
 --envs_file trainEnvironments.pkl --path_data_file trainPaths.pkl \
 --trained_model_path ./models/sample/voxel_01_1000/ \
 --batch_size 100 --learning_rate 0.001  --num_epochs 201 \
---enc_input_size 32 --enc_output_size 128 --mlp_input_size 142 --mlp_output_size 7 --AE_type voxel --NP 1000 \
+--enc_input_size 128 --enc_output_size 128 --mlp_input_size 142 --mlp_output_size 7 --AE_type voxel --NP 1000 \
 --device 2
