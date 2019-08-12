c097532299be79b48b365b12623b2eecb446da8e
MiaoDragon <innocentmyl@gmail.com>
2019-08-12 15:38:53 -0700

    added training for voxel5 on 64 grids

--- torch_architectures.py
+++ torch_architectures.py
@@ -140,6 +140,34 @@ class VoxelEncoder4(nn.Module):
         x = self.head(x)
         return x
 
+class VoxelEncoder5(nn.Module):
+    # ref: https://github.com/lxxue/voxnet-pytorch/blob/master/models/voxnet.py
+    def __init__(self, input_size, output_size):
+        super(VoxelEncoder5, self).__init__()
+        input_size = [input_size, input_size, input_size]
+        self.encoder = nn.Sequential(
+            nn.Conv3d(in_channels=1, out_channels=32, kernel_size=[7,7,7], stride=[2,2,2]),
+            nn.PReLU(),
+            nn.Conv3d(in_channels=32, out_channels=32, kernel_size=[5,5,5], stride=[2,2,2]),
+            nn.PReLU(),
+            nn.Conv3d(in_channels=32, out_channels=32, kernel_size=[3,3,3], stride=[1,1,1]),
+            nn.PReLU()
+            #nn.MaxPool3d(kernel_size=[2,2,2], stride=[2,2,2])
+        )
+        x = self.encoder(torch.autograd.Variable(torch.rand([1, 1] + input_size)))
+        first_fc_in_features = 1
+        for n in x.size()[1:]:
+            first_fc_in_features *= n
+        self.head = nn.Sequential(
+            nn.Linear(first_fc_in_features, 128),
+            nn.PReLU(),
+            nn.Linear(128, output_size)
+        )
+    def forward(self, x):
+        x = self.encoder(x)
+        x = x.view(x.size(0), -1)
+        x = self.head(x)
+        return x
 
 
 
---
+++ torch_run_training_voxel3_64_001.sh
@@ -0,0 +1,7 @@
+python torch_train.py \
+--env_data_path ./env/environment_data/ --path_data_path ./data/train/paths/ --pointcloud_data_path ./data/train/pcd/ \
+--envs_file trainEnvironments.pkl --path_data_file trainPaths.pkl \
+--trained_model_path ./models/sample/voxel3_64_001/ \
+--batch_size 100 --learning_rate 0.001  --num_epochs 400 \
+--enc_input_size 64 --enc_vox_size 64 --enc_output_size 128 --mlp_input_size 142 --mlp_output_size 7 --AE_type voxel3 --N 1 --NP 100 \
+--device 3 --exp_name voxel3
