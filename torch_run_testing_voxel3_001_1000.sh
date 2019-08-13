66415d9ae3bb23291e5356db61e37582e8fc99eb
MiaoDragon <innocentmyl@gmail.com>
2019-08-13 11:53:12 -0700

    try 120 epoch of voxelnet3

--- torch_run_testing_voxel3_001_1000.sh
+++ torch_run_testing_voxel3_001_1000.sh
@@ -1,5 +1,5 @@
 python torch_test.py \
 --env_data_path ./env/environment_data/ --path_data_path ./test_data/test/paths/ --pointcloud_data_path ./test_data/test/pcd/ \
 --envs_file trainEnvironments.pkl --path_data_file trainEnvironments_testPaths.pkl \
---model_path ./models/sample/voxel3_001_1000/ --mlp_model_name mlp_PReLU_ae_dd80.pkl --enc_model_name cae_encoder_80.pkl --experiment_name test_experiment \
+--model_path ./models/sample/voxel3_001_1000/ --mlp_model_name mlp_PReLU_ae_dd120.pkl --enc_model_name cae_encoder_120.pkl --experiment_name test_experiment \
 --AE_type voxel3 --enc_input_size 32 --enc_output_size 128 --mlp_input_size 142 --mlp_output_size 7 --device 0 --exp_name voxel3 --N 10
